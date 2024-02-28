44ef17f6427300b5ee0b7c6130dd2d494f1e99dc
Tyler Crumpton <tyler@crump.space>
2024-02-27 23:50:17 -0500

    Trim trailing whitespace

--- firmware/circuitpy-code/m8b/hardware/shake.py
+++ firmware/circuitpy-code/m8b/hardware/shake.py
@@ -15,19 +15,19 @@ class ShakeEvent(Event):
 class Shake:
     def __init__(self):
         self.shake_counter = Counter(SHAKE_PIN, pull=Pull.UP)
-     
+
 
     def get_events(self) -> list[ShakeEvent]:
         if self.shake_counter.count > 0:
             self.shake_counter.reset()
             return [ShakeEvent()]
         return []
-    
+
     def get_pin_alarm(self):
         # Need to release the pin from the counter to use it as an alarm pin
         self.shake_counter.deinit()
         return alarm.pin.PinAlarm(SHAKE_PIN, pull=Pull.UP, value=False)
-        
+
 
 
 shake = Shake()
